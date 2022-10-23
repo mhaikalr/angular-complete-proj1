632778b393ea3735c2947684b25fdfc138558003
Haikal Rosman <mhaikalr1992@gmail.com>
2022-10-23 21:57:25 +0800

    165

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeDetailsComponent } from './recipes/recipe-details/recipe-details.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 const appRoutes: Routes = [
     {
@@ -20,8 +21,16 @@ const appRoutes: Routes = [
                 component: RecipeStartComponent
             },
             {
+                path: 'new',
+                component: RecipeEditComponent
+            },
+            {
                 path: ':id',
                 component: RecipeDetailsComponent
+            },
+            {
+                path: ':id/edit',
+                component: RecipeEditComponent
             }
         ]
     },
